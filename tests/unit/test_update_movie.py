12956c809c41d5372d8de556381fa43b5f4498fe
WKirkJr <73005151+WKirklandJr@users.noreply.github.com>
2023-03-21 00:26:31 -0400

    Got tired but started for testing

--- tests/unit/test_update_movie.py
+++ tests/unit/test_update_movie.py
@@ -1,1 +1,6 @@
 # TODO: Feature 5
+# first import the function we want to test via unit test
+from src.repositories.movie_repository import get_movie_repository
+
+# create our test function
+# def test_edit_unit():
