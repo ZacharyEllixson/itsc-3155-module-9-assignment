1acf8a73bed3f7d91ea0c9facf07e081c2a9f700
Aaron Lobo <aplobo12@gmail.com>
2023-03-22 23:28:47 -0400

    updated unit test

--- tests/unit/test_create_movie.py
+++ tests/unit/test_create_movie.py
@@ -4,7 +4,7 @@ from src.repositories.movie_repository import get_movie_repository
 def test_create_movie():
     get_movie_repository().create_movie('movie', 'director', 2)
     movie = get_movie_repository().get_all_movies()
-    movie_id = list(movie.keys())[0]
+    movie_id = list(movie.keys())[1]
     assert movie[movie_id].title == 'movie'
     assert movie[movie_id].director == 'director'
     assert movie[movie_id].rating == 2
