71ff7ec097a827c62dae8fab913bef1a5440b0d5
jkingg <jaskin233@gmail.com>
2023-03-22 23:14:49 -0400

    all movie tests

--- tests/unit/test_get_all_movies.py
+++ tests/unit/test_get_all_movies.py
@@ -1,1 +1,7 @@
 # TODO: Feature 1
+from src.repositories.movie_repository import get_movie_repository
+from src.models.movie import Movie
+from app import list_all_movies
+
+def test_get_all_movies():
+    get_movie_repository().create_movie('Movie Name', 'Director Name', 1)
