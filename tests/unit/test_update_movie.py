88faaecbc5638eaecdc999629a4eb94805132a81
jkingg <jaskin233@gmail.com>
2023-03-22 22:51:38 -0400

    committing

--- tests/unit/test_update_movie.py
+++ tests/unit/test_update_movie.py
@@ -1,11 +1,2 @@
 
-# create our test function
-
-
-def test_edit_unit(test_app):
-    test_app = app.test_client
-    response = test_app.get('/movies/<int:movie_id>')
-    assert response.status_coude == 200
-
-# Resources
-# https://www.youtube.com/watch?v=bToSg1TSpMA
+# TODO 5
