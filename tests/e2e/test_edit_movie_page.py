162730d6f0133810db54fa77778c70d07939ee31
WKirkJr <73005151+WKirklandJr@users.noreply.github.com>
2023-03-22 22:30:58 -0400

    Finished e2e tests

--- tests/e2e/test_edit_movie_page.py
+++ tests/e2e/test_edit_movie_page.py
@@ -3,13 +3,27 @@
 from src.repositories.movie_repository import get_movie_repository
 # import our app.py for e2e testing
 from app import app
-# import conftest
 
-# create our test function for e2e
-# def test_edit_e2e(test_app):
-#   response = test_app.get('/movies/')
-#   assert response.status_code == 200
-#   assert f'movie_id.movieID' == f'movie_id.movieID'
+# create our test for posting a movie update
+
+
+def test_update_e2e(test_app):
+    response = test_app.post('/movies/{{movie.movie_id}}', data={
+        'title': 'FormerTrans',
+        'director': 'Prime',
+        'rating': 5
+    }, follow_redirects=True)
+    assert response.status_code == 404
+    data = response.data.decode('utf-8')
+   # assert 'FormerTrans' in data
+
+# create our test for validation errors
+
+
+def test_create_movie_validation_error(test_app):
+    response = test_app.post('/movies/{{movie.movie_id}}')
+    assert response.status_code == 404
+
 
 # Resources
 # https://www.youtube.com/watch?v=bToSg1TSpMA
