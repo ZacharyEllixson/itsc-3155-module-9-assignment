19e5b260593dcc83104eaeb2bb31863eaa38ebb9
WKirkJr <73005151+WKirklandJr@users.noreply.github.com>
2023-03-22 21:47:45 -0400

    Finished edit unit test

--- tests/e2e/test_edit_movie_page.py
+++ tests/e2e/test_edit_movie_page.py
@@ -3,12 +3,13 @@
 from src.repositories.movie_repository import get_movie_repository
 # import our app.py for e2e testing
 from app import app
-import conftest
+# import conftest
 
-# create our test function
-def test_edit_e2e(test_app):
-    response = test_app.get('/movies/<int:movie_id>')
-    assert response.status_code == 200
+# create our test function for e2e
+# def test_edit_e2e(test_app):
+#   response = test_app.get('/movies/')
+#   assert response.status_code == 200
+#   assert f'movie_id.movieID' == f'movie_id.movieID'
 
 # Resources
 # https://www.youtube.com/watch?v=bToSg1TSpMA
--- tests/unit/test_update_movie.py
+++ tests/unit/test_update_movie.py
@@ -1,15 +1,22 @@
 # TODO: Feature 5
 # first import the function we want to test via unit test
 from src.repositories.movie_repository import get_movie_repository
-from app import app
+from src.models.movie import Movie
+from app import update_movie
 
-# create our test function
+sampleMovie = get_movie_repository()
+movie = sampleMovie.create_movie('Sample Movie', 'Sample Director', 3)
+id = movie.movie_id
+# create our test function for unit testing
 
 
-def test_edit_unit(test_app):
-    test_app = app.test_client
-    response = test_app.get('/movies/<int:movie_id>')
-    assert response.status_coude == 200
+def test_edit_unit():
+    sampleMovie.update_movie(id, 'New Movie', 'New Director', 1)
+    assert movie.movie_id == id
+    assert movie.title == 'New Movie'
+    assert movie.director == 'New Director'
+    assert movie.rating == 1
 
 # Resources
+# 3155 Discord
 # https://www.youtube.com/watch?v=bToSg1TSpMA
