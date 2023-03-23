1999c6e3ec85f9c43d540fc8bb9b700248781d56
jkingg <jaskin233@gmail.com>
2023-03-22 23:07:23 -0400

    merging

--- tests/e2e/test_all_movies_page.py
+++ tests/e2e/test_all_movies_page.py
@@ -1,1 +1,14 @@
 # TODO: Feature 1
+from app import app
+from src.repositories.movie_repository import get_movie_repository
+
+def test_all_movies_page(test_app):
+
+    response = test_app.get('/movies')
+    assert response.status_code == 200
+    assert b'<h1>There are currently no movies listed.</h1>' in response.data
+
+    get_movie_repository().create_movie('Movie Name', 'Director', 1)
+    response = test_app.get('/movies')
+    assert response.status_code == 200
+    assert b'<td>Movie Name</td>'
