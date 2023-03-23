15a35d26a16815b7023961e911f8642628fcee07
Jose Lara-Urbina <joselaraurb@gmail.com>
2023-03-22 23:18:05 -0400

    Fixing spelling error

--- tests/unit/test_get_movie_by_id.py
+++ tests/unit/test_get_movie_by_id.py
@@ -2,11 +2,11 @@
 from src.repositories.movie_repository import get_movie_repository
 
 def test_get_movie_by_id():
-    movie_repositoy = get_movie_repository()
-    movie = movie_repositoy.create_movie('movie_name', 'director_name', 5)
+    movie_repository = get_movie_repository()
+    movie = movie_repository.create_movie('movie_name', 'director_name', 5)
     movie_id = movie.movie_id
 
-    get_by_id_movie = movie_repositoy.get_movie_by_id(movie_id)
+    get_by_id_movie = movie_repository.get_movie_by_id(movie_id)
 
     assert get_by_id_movie.movie_id == movie_id
     assert get_by_id_movie.title == 'movie_name'
