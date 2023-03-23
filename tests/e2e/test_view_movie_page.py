56c6d7f03c37ff3c48ea1381f8ee1122d67fce6a
Jose Lara-Urbina <joselaraurb@gmail.com>
2023-03-22 22:47:24 -0400

    Adding e2e testing for feature 4 and unit testing for the get_movie_by_id method

--- tests/e2e/test_view_movie_page.py
+++ tests/e2e/test_view_movie_page.py
@@ -1,1 +1,17 @@
 # TODO: Feature 4
+from src.repositories.movie_repository import get_movie_repository
+
+def test_view_movie_page(test_app):
+    movie_repository = get_movie_repository()
+    movie = movie_repository.create_movie('movie_name', 'director_name', 5)
+    movie_id = movie.movie_id
+
+    response = test_app.get(f"/movies/{movie_id}")
+
+    assert response.status_code == 200
+
+    data = response.data.decode('utf-8')
+
+    assert '<h1 class="mb-5">movie_name</h1>' in data
+    assert '<p>Director: director_name</p>' in data
+    assert '<p>Rating: 5</p>' in data
--- tests/unit/test_get_movie_by_id.py
+++ tests/unit/test_get_movie_by_id.py
@@ -1,1 +1,14 @@
 # TODO: Feature 4
+from src.repositories.movie_repository import get_movie_repository
+
+def test_get_movie_by_id():
+    movie_repositoy = get_movie_repository()
+    movie = movie_repositoy.create_movie('movie_name', 'director_name', 5)
+    movie_id = movie.movie_id
+
+    get_by_id_movie = movie_repositoy.get_movie_by_id(movie_id)
+
+    assert get_by_id_movie.movie_id == movie_id
+    assert get_by_id_movie.title == 'movie_name'
+    assert get_by_id_movie.director == 'director_name'
+    assert get_by_id_movie.rating == 5
